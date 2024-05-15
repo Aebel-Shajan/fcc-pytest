f423fe85bc14d4aabac2a106537e81ff00e9b155
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-15 20:14:13 +0100

    Add test for adding strings in my_functions module

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -5,6 +5,10 @@ def test_add():
   result = my_functions.add(1, 4)
   assert result == 5
 
+def test_add_strings():
+  result = my_functions.add("i like ", "burgers.")
+  assert result == "i like burgers."
+
 def test_divide():
   result = my_functions.divide(10, 5)
   assert result == 2
