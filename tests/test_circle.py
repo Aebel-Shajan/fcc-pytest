97856aa8beb2a28b531ff1615dae606944ede294
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-16 14:59:17 +0100

    Add tests for area and perimeter of a circle

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,6 +1,22 @@
 import pytest
 import source.shapes as shapes
+import math
 
 class TestCircle:
-  def test_one(self):
-    assert True
+  def setup_method(self, method):
+    print(f"Setting up {method}")
+    self.circle = shapes.Circle(10)
+
+  def teardown_method(self, method):
+    print(f"Tearing down {method}")
+    del self.circle
+
+  def test_area(self):
+    assert self.circle.area() == math.pi * self.circle.radius ** 2
+
+  def test_perimeter(self):
+    result = self.circle.perimeter()
+    expected = 2 * math.pi * self.circle.radius
+    assert result == expected
+
+  
