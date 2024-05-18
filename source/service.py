b25cf8587008634e1f4ac52f6a87fa43c00fb3c3
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-18 19:26:45 +0100

    Add get_users method which makes call from an api

--- source/service.py
+++ source/service.py
@@ -1,3 +1,5 @@
+import requests
+
 database = {
     1: "Alice",
     2: "Bob",
@@ -6,3 +8,10 @@ database = {
 
 def get_user_from_db(user_id):
     return database.get(user_id)
+
+def get_users():
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    if response.status_code == 200:
+        return response.json()
+    
+    raise requests.HTTPError
