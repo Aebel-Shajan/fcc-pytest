31ece46ead52ea6f5fb32cb9e16bf0a544cad9aa
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-18 19:39:37 +0100

    Add test for get_users making use of @mock.patch to mock the api request in get_users

--- tests/test_service.py
+++ tests/test_service.py
@@ -9,3 +9,12 @@ def test_get_user_from_db(mock_get_user_from_db):
     user_name = service.get_user_from_db(1)
     assert user_name == "Mocked Alice"
 
+@mock.patch("requests.get")
+# whenever requests.get() is called withtin source.service.get_users() then replace that with the mock_get
+def test_get_users(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"id": 1, "name": "John Doe"}
+    mock_get.return_value = mock_response
+    data = service.get_users()
+    assert data == {"id": 1, "name": "John Doe"}
