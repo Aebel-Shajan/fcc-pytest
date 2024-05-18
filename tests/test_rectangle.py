60e1c42855c057944d4e2062155d6b6b5089354d
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-18 18:30:12 +0100

    Add rectangle equality test

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -5,8 +5,15 @@ import source.shapes as shapes
 def my_rectangle():
     return shapes.Rectangle(10, 20)
 
+@pytest.fixture()
+def weird_rectangle():
+    return shapes.Rectangle(5, 6)
+
 def test_area(my_rectangle):
     assert my_rectangle.area() == 10 * 20
 
 def test_perimeter(my_rectangle):
     assert my_rectangle.perimeter() == (10 * 2) + (20 * 2)
+
+def test_not_equal(my_rectangle, weird_rectangle):
+    assert my_rectangle != weird_rectangle
