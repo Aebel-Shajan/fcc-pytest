97c5d2f3977ed2f829f29333938582f9800cd938
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-18 18:50:32 +0100

    Add parametrized tests for square class using @pytest.mark.parametrize

---
+++ tests/test_square.py
@@ -0,0 +1,6 @@
+import pytest
+import source.shapes as shapes
+
+@pytest.mark.parametrize("side_length, expected_area", [(5, 25), (4, 16), (9, 81)])
+def test_multiple_square_areas(side_length, expected_area):
+    assert shapes.Shape(side_length).area() == expected_area
