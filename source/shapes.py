e60e5591856eb2ace7d5c469167c3fd2720425f9
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-18 18:47:12 +0100

    Add square class to shapes.py

--- source/shapes.py
+++ source/shapes.py
@@ -34,3 +34,7 @@ class Rectangle(Shape):
   
   def perimeter(self):
     return 2 * (self.length + self.width)
+
+class Square(Rectangle):
+  def __init__(self, side_length):
+    super().__init__(side_length, side_length)
