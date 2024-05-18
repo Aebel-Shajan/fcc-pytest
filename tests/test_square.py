dbd846c065b3b486196321b022f55aaee2f9aaa3
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-18 18:54:20 +0100

    Add parametrized perimeter tests for square

--- tests/test_square.py
+++ tests/test_square.py
@@ -4,3 +4,7 @@ import source.shapes as shapes
 @pytest.mark.parametrize("side_length, expected_area", [(5, 25), (4, 16), (9, 81)])
 def test_multiple_square_areas(side_length, expected_area):
     assert shapes.Square(side_length).area() == expected_area
+
+@pytest.mark.parametrize("side_length, expected_perimeter", [(3, 12), (4, 16), (5, 20)])
+def test_multiple_perimeters(side_length, expected_perimeter):
+    assert shapes.Square(side_length).perimeter() == expected_perimeter
