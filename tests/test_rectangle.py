162c6844fd418571ea188ea75d2cb9c8cada0868
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-18 18:11:46 +0100

    Add fixture to rectangle tests

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -1,10 +1,12 @@
 import pytest
 import source.shapes as shapes
 
-def test_area():
-    rectangle = shapes.Rectangle(10, 20)
-    assert rectangle.area() == 10 * 20
+@pytest.fixture
+def my_rectangle():
+    return shapes.Rectangle(10, 20)
 
-def test_perimater():
-    rectangle = shapes.Rectangle(10, 20)
-    assert rectangle.perimeter() == (10 * 2) + (20 * 2)
+def test_area(my_rectangle):
+    assert my_rectangle.area() == 10 * 20
+
+def test_perimeter(my_rectangle):
+    assert my_rectangle.perimeter() == (10 * 2) + (20 * 2)
