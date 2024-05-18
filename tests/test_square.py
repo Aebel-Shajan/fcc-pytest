5144f095128f32075b640f3387ac1299a84e6673
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-18 18:51:31 +0100

    Fix incorrect constructor for square

--- tests/test_square.py
+++ tests/test_square.py
@@ -3,4 +3,4 @@ import source.shapes as shapes
 
 @pytest.mark.parametrize("side_length, expected_area", [(5, 25), (4, 16), (9, 81)])
 def test_multiple_square_areas(side_length, expected_area):
-    assert shapes.Shape(side_length).area() == expected_area
+    assert shapes.Square(side_length).area() == expected_area
