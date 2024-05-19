14968effbc6c5aca2d324e8cf8ec9528553df31c
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-19 09:11:40 +0100

    Add test to mock 400 response error

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,6 +1,7 @@
 import pytest
 import source.service as service
 import unittest.mock as mock # Already comes with python
+import requests
 
 
 @mock.patch("source.service.get_user_from_db")
@@ -18,3 +19,11 @@ def test_get_users(mock_get):
     mock_get.return_value = mock_response
     data = service.get_users()
     assert data == {"id": 1, "name": "John Doe"}
+
+@mock.patch("requests.get")
+def test_get_users_error(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 400
+    mock_get.return_value = mock_response
+    with pytest.raises(requests.HTTPError):
+        service.get_users()
